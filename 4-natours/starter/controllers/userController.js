673bb51015b0bc41aaa587b0b5ac3ce10c07718d
Pierre Felgines <pierre.felgines@fabernovel.com>
2023-01-07 21:47:58 +0100

    Update user photo

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -1,8 +1,35 @@
+const multer = require('multer');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const filterObj = require('../utils/filterObj');
 const factory = require('./handlerFactory');
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'public/img/users');
+  },
+  filename: (req, file, callback) => {
+    const extension = file.mimetype.split('/')[1];
+    callback(null, `user-${req.user.id}-${Date.now()}.${extension}`);
+  },
+});
+const multerFilter = (req, file, callback) => {
+  if (file.mimetype.startsWith('image/')) {
+    callback(null, true);
+  } else {
+    callback(
+      new AppError('Not an image, please only upload images', 400),
+      false
+    );
+  }
+};
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+const uploadUserPhoto = upload.single('photo');
 
 const updateMe = catchAsync(async (req, res, next) => {
   // Create error if user tries to update the password
@@ -16,6 +43,12 @@ const updateMe = catchAsync(async (req, res, next) => {
   }
   // Filter out unwanted fields
   const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // Add photo if needed
+  if (req.file) {
+    filteredBody.photo = req.file.filename;
+  }
+
   // Update the user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true, // returns new object
@@ -54,6 +87,7 @@ module.exports = {
   createUser,
   updateUser,
   deleteUser,
+  uploadUserPhoto,
   updateMe,
   deleteMe,
   getMe,
